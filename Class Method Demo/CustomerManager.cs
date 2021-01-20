05d76cd792dfa2dd0e2de4e66870969611f8c420
cemilcan sağdıç <cemilcansagdic@hotmail.com>
2021-01-20 16:03:10 +0300

    commit2

--- Class Method Demo/CustomerManager.cs
+++ Class Method Demo/CustomerManager.cs
@@ -6,5 +6,24 @@ namespace Class_Method_Demo
 {
     class CustomerManager
     {
+        public void Add(Customer customer, List<Customer> customers)
+        {
+            customers.Add(customer);
+        }
+
+        public void List(List<Customer> customers)
+        {
+            foreach(Customer item in customers)
+            {
+                Console.WriteLine(item.FullName);
+                Console.WriteLine(item.TCKN);
+                Console.WriteLine(item.Occupation);
+            }
+        }
+
+        public void Delete(Customer customer, List<Customer> customers)
+        {
+            customers.Remove(customer);
+        }
     }
 }
